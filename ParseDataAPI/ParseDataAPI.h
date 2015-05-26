069c64325d147cc556f3174ab9e2177c3113726f
Mariam <mariam.dost09@gmail.com>
2015-05-26 15:16:59 -0600

    Initial Commit

---
+++ ParseDataAPI/ParseDataAPI.h
@@ -0,0 +1,13 @@
+//
+//  ParseDataAPI.h
+//  ParseDataAPI
+//
+//  Created by Mariam Dost on 2015-05-26.
+//  Copyright (c) 2015 Mariam Dost. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ParseDataAPI : NSObject
+
+@end
---
+++ ParseDataAPI/ParseDataAPI.m
@@ -0,0 +1,13 @@
+//
+//  ParseDataAPI.m
+//  ParseDataAPI
+//
+//  Created by Mariam Dost on 2015-05-26.
+//  Copyright (c) 2015 Mariam Dost. All rights reserved.
+//
+
+#import "ParseDataAPI.h"
+
+@implementation ParseDataAPI
+
+@end
