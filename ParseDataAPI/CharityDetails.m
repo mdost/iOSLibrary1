67110d8b29ce81cecd25adc6bbad2fc2247426f8
Mariam <mariam.dost09@gmail.com>
2015-06-02 10:56:05 -0600

    added parameters to charityDetails

--- ParseDataAPI/CharityDetails.m
+++ ParseDataAPI/CharityDetails.m
@@ -31,6 +31,41 @@
         _CharitySubType=parameters[@"CharitySubType"];
         _CharityType=parameters[@"CharityType"];
         _CharityUrl=parameters[@"CharityUrl"];
+        _Comments=parameters[@"Comments"];
+        _Competitors=parameters[@"Competitors"];
+        _DateOfStatus=parameters[@"DateOfStatus"];
+        _MajorDonors=parameters[@"MajorDonors"];
+        _MissionStatement=parameters[@"MissionStatement"];
+        _UniqueBlurb=parameters[@"UniqueBlurb"];
+        _EmerFundDescrip=parameters[@"EmerFundDescrip"];
+        _ExecutiveDirector=parameters[@"ExecutiveDirector"];
+        _ExistBlurb=parameters[@"ExistBlurb"];
+        _WhatIsNeeded=parameters[@"WhatIsNeeded"];
+        _Notes=parameters[@"Notes"];
+        _NumOfStaff=parameters[@"NumOfStaff"];
+        _NumOfVolunteers=parameters[@"NumOfVolunteers"];
+        _OfferingsDifferedDescrip=parameters[@"OfferingsDifferedDescrip"];
+        _OperatingBudgetYear1=parameters[@"OperatingBudgetYear1"];
+        _OperatingBudgetYear2=parameters[@"OperatingBudgetYear2"];
+        _OperatingBudgetYear3=parameters[@"OperatingBudgetYear3"];
+        _OrganizationSize=parameters[@"OrganizationSize"];
+        _OverheadExpensesDescription=parameters[@"OverheadExpensesDescription"];
+        _OverheadExpensesPercent=parameters[@"OverheadExpensesPercent"];
+        _PeopleImpactedByOrganization=parameters[@"PeopleImpactedByOrganization"];
+        _PeopleImpactedByPrograms=parameters[@"PeopleImpactedByPrograms"];
+        _PublicPerception=parameters[@"PublicPerception"];
+        _PressReleases=parameters[@"PressReleases"];
+        _ProcessDescrip=parameters[@"ProcessDescrip"];
+        _ProgramDescription=parameters[@"ProgramDescription"];
+        _KeyIssuesDescrip=parameters[@"KeyIssuesDescrip"];
+        _KeyIssuesPercent=parameters[@"KeyIssuesPercent"];
+        _Keywords=parameters[@"Keywords"];
+        _LogoFilename=parameters[@"LogoFilename"];
+        _HasCodeOfEthicalFundraising=parameters[@"HasCodeOfEthicalFundraising"];
+        _HowOfferingsDiffered=parameters[@"HowOfferingsDiffered"];
+        _IndividualPrograms=parameters[@"IndividualPrograms"];
+        _InKindDonDescrip=parameters[@"InKindDonDescrip"];
+        
         
     }
     
