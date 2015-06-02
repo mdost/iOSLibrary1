278120cf8439dda3f36f6d855d3e9e96d47254ce
Mariam <mariam.dost09@gmail.com>
2015-06-02 16:05:05 -0600

    error handling

---
+++ ParseDataAPI/GiveAPI.h
@@ -0,0 +1,13 @@
+//
+//  GiveAPI.h
+//  ParseDataAPI
+//
+//  Created by Mariam Dost on 2015-06-02.
+//  Copyright (c) 2015 Mariam Dost. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GiveAPI : NSObject
+
+@end
---
+++ ParseDataAPI/GiveAPI.m
@@ -0,0 +1,13 @@
+//
+//  GiveAPI.m
+//  ParseDataAPI
+//
+//  Created by Mariam Dost on 2015-06-02.
+//  Copyright (c) 2015 Mariam Dost. All rights reserved.
+//
+
+#import "GiveAPI.h"
+
+@implementation GiveAPI
+
+@end
